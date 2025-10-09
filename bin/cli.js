e93d4c7002730465798fc94a0dbac6d5ecb580b8
bill <bill@bill.bill>
2025-10-09 16:33:47 -0700

    fixed lib and cli separation

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,13 @@
 #!/usr/bin/env node
-import { runCLI } from '../index.js';
 
-runCLI();
+import path from "path";
+import {eztree} from "../index.js";
+
+function runCLI(argv) {
+    const targetPath = argv[argv.length - 1] === argv[1] ? process.cwd() : path.resolve(argv[argv.length - 1]);
+    console.log(
+        eztree(targetPath)
+    );
+}
+
+runCLI(process.argv);
--- index.js
+++ index.js
@@ -28,19 +28,6 @@ export function eztree(dir, prefix = '') {
   return output;
 }
 
-// CLI entry point
-export function runCLI(argv = process.argv) {
-  const targetPath = argv[argv.length - 1] === argv[1] ? process.cwd() : path.resolve(argv[argv.length - 1]);
-  console.log(
-    eztree(targetPath)
-  );
-}
-
-// Only run CLI if executed directly
-if (process.argv[1] === (process.env._ || process.argv[1])) {
-  runCLI(process.argv);
-}
-
 //
 // Integration notes:
 // - Make sure to run `chmod +x index.js` if on Unix/Mac.
